c1be2a38abe5ff5f0c9011d0d2e7b8daf55ca197
Jorgeacus <marcusja@zohomail.eu>
2026-08-04 13:31:43 +0100

    post.sh: adicionar meta author + link 'Definições de cookies' ao template; aplicar ao artigo do Telegram

--- scripts/post.sh
+++ scripts/post.sh
@@ -80,6 +80,7 @@ cat > "$POST_DIR/index.html" << HTMLEOF
   <link rel="stylesheet" href="/assets/css/style.css?v=20260804">
   <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>">
   <meta name="google-adsense-account" content="ca-pub-3717814491008089">
+  <meta name="author" content="${AUTHOR}">
   <script type="application/ld+json">
 {
   "@context": "https://schema.org",
@@ -167,6 +168,7 @@ cat > "$POST_DIR/index.html" << HTMLEOF
             <li><a href="/politica-de-cookies/">Cookies</a></li>
             <li><a href="/termos/">Termos</a></li>
             <li><a href="/contactos/">Contactos</a></li>
+            <li><a href="#" id="cookie-settings" onclick="window.MarctechJACookieConsent&&window.MarctechJACookieConsent.show();return false;">Definições de cookies</a></li>
           </ul>
         </div>
         <div class="footer-col">
